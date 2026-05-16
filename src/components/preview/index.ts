0b30c0372c24a01c96009245e6910df294a3d59c
HuyHoangDevVN <nguyenthehuyhoang2002@gmail.com>
2026-05-16 07:15:52 +0700

    feat: add PDF.js viewer for enhanced PDF rendering and support in file preview

--- src/components/preview/index.ts
+++ src/components/preview/index.ts
@@ -2,4 +2,5 @@ export { ArchivePreview } from "./ArchivePreview";
 export { CsvPreview } from "./CsvPreview";
 export { DocumentPreview } from "./DocumentPreview";
 export { PdfPreview } from "./PdfPreview";
+export { PdfJsViewer } from "./PdfJsViewer";
 export { TextPreview } from "./TextPreview";
