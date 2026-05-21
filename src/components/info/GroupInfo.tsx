c1f1f0f0f8d92166c1ee8514c80a0a1ac79151b8
HuyHoangDevVN <nguyenthehuyhoang2002@gmail.com>
2026-05-21 19:12:52 +0700

    fix: remove unnecessary conversationId from completeUpload call

--- src/components/info/GroupInfo.tsx
+++ src/components/info/GroupInfo.tsx
@@ -541,7 +541,6 @@ export const GroupInfo: React.FC<GroupInfoProps> = ({
         setGroupAvatarStage("completing");
         const completed = await uploadClient.completeUpload({
           uploadId: reserved.uploadId,
-          conversationId: conversation.id,
           objectKey: reserved.objectKey,
         });
 
